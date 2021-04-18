cc15854c1ede91b15d76ff27a08cf0d74816fc57
Jon Kivinen <226881+jkiv@users.noreply.github.com>
2021-04-18 15:05:02 -0400

    Added sha_round_usage_tb to Makefile.
    Replaced ='s with <= in sha_unit_usage_tb.
    
    Addresses issue #17.

--- src/tests/sha_round_usage_tb.v
+++ src/tests/sha_round_usage_tb.v
@@ -37,28 +37,28 @@ module sha_round_usage_tb();
       // Set up case data
 
       // Case 0
-      S0_case[0] = 256'h6a09e667_bb67ae85_3c6ef372_a54ff53a_510e527f_9b05688c_1f83d9ab_5be0cd19;
-      Kt_case[0] = 32'h428a2f98;
-      Wt_case[0] = 32'h61626380;
-      S1_expected[0] = 256'h5d6aebcd_6a09e667_bb67ae85_3c6ef372_fa2a4622_510e527f_9b05688c_1f83d9ab;
+      S0_case[0] <= 256'h6a09e667_bb67ae85_3c6ef372_a54ff53a_510e527f_9b05688c_1f83d9ab_5be0cd19;
+      Kt_case[0] <= 32'h428a2f98;
+      Wt_case[0] <= 32'h61626380;
+      S1_expected[0] <= 256'h5d6aebcd_6a09e667_bb67ae85_3c6ef372_fa2a4622_510e527f_9b05688c_1f83d9ab;
 
       // Case 1
-      S0_case[1] = 256'h04d24d6c_b85e2ce9_b6ae8fff_ffb70472_948d25b6_961f4894_b21bad3d_6d83bfc6;
-      Kt_case[1] = 32'hbef9a3f7;
-      Wt_case[1] = 32'heeaba2cc;
-      S1_expected[1] = 256'hd39a2165_04d24d6c_b85e2ce9_b6ae8fff_fb121210_948d25b6_961f4894_b21bad3d;
+      S0_case[1] <= 256'h04d24d6c_b85e2ce9_b6ae8fff_ffb70472_948d25b6_961f4894_b21bad3d_6d83bfc6;
+      Kt_case[1] <= 32'hbef9a3f7;
+      Wt_case[1] <= 32'heeaba2cc;
+      S1_expected[1] <= 256'hd39a2165_04d24d6c_b85e2ce9_b6ae8fff_fb121210_948d25b6_961f4894_b21bad3d;
 
       // Case 2
-      S0_case[2] = 256'b0;
-      Kt_case[2] = 256'b0;
-      Wt_case[2] = 256'b0;
-      S1_expected[2] = 256'b0;
+      S0_case[2] <= 256'b0;
+      Kt_case[2] <= 256'b0;
+      Wt_case[2] <= 256'b0;
+      S1_expected[2] <= 256'b0;
 
       for (i = 0; i < NUMBER_OF_CASES; i = i + 1)
         begin
-          S0 = S0_case[i];
-          Kt = Kt_case[i];
-          Wt = Wt_case[i];
+          S0 <= S0_case[i];
+          Kt <= Kt_case[i];
+          Wt <= Wt_case[i];
           #1;
 
           if (S1 == S1_expected[i])
